38787dd9499add5b84046ed7b63786af0d4dded5
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-03-12 17:11:37 +0300

    chore: add expo-dev-client

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Stack } from 'expo-router'
+import { Text, View } from 'react-native'
 
-export default function RootLayout() {
-	return <Stack />
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Text>Edit app/index.tsx to edit this screen.</Text>
+		</View>
+	)
 }
