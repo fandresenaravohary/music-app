539323858656f945f0816564ba742a014a72be7d
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-03-14 19:35:58 +0300

    chore: add useSetupTrackPlayer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,22 @@
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { SplashScreen } from 'expo-router'
+import { useCallback } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 const App = () => {
+
+	const handleTrackPlayerLoaded = useCallback(() => {
+		SplashScreen.hideAsync()
+	}, [])
+
+	useSetupTrackPlayer({
+		onLoad: handleTrackPlayerLoaded,
+	})
+
 	return (
 		<SafeAreaProvider>
 			<RootNavigation />
